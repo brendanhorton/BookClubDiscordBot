40695702dd4fd9deca43c47e4ae294ee9ebc3f2c
Brendan  Horton <brendanhorton@Brendans-MacBook-Pro.local>
2020-12-06 15:50:12 -0500

    added halfway and end of month reminders, next is creating book hash to save book for each month

--- Booker.py
+++ Booker.py
@@ -14,54 +14,47 @@ currentDate = datetime.datetime.now()
 #if expanded to work for others would need to have a different solution to finding channel id
 CHANNEL_ID = 784457938551046167
 
+#want to eventually create hash to keep track of books for each month -- hardcoding for now
+bookOfTheMonth = 'Death on the Nile'
+
 # this event makes sure bot is ready and will output when it is
 #want to change bot so it displays the current months book // not sure how 
 #maybe i need to use a datetime object and conditionals
 #if month == x then output y as name of activity
 #can have large hash with book for each month
 #expansion would include allowing user to set books for each month per instance of bot
-# doable
+#doable
 @bot.event
 async def on_ready():
-    print('bot running')
-    await bot.change_presence(activity= discord.Activity(type = discord.ActivityType.listening, name = "Death on the Nile"))
+    print('Booker started reading!')
+    await bot.change_presence(activity= discord.Activity(type = discord.ActivityType.listening, name = bookOfTheMonth))
 
 #when someone types test bot returns testing 123
+#need to translate datetime into written month -- shouldnt be hard -- might be included in module quick google
 @bot.event
 async def on_message(message):
     if message.content == '!currentbook':
-        await message.channel.send('The book for the month of December is Death on the Nile')
+        await message.channel.send(f'The book for the month of December is {bookOfTheMonth}')
     
     await bot.process_commands(message)
 
-#simpel test command to learn discordloops
-#runs every 10sec
-#must have the before loop check to ready or it doesnt work
-# @tasks.loop(seconds=10)
-# async def testingLoop():
-#     message_channel= bot.get_channel(CHANNEL_ID)
-#     await message_channel.send("10 Second Bot Initiated")
 
-# @testingLoop.before_loop
-# async def before():
-#     await bot.wait_until_ready()
-#     print("Finished waiting")
-
-#whiteboard this
-#want this loop to run once a week, provide motivation and remind for reading 
-#ask users -- are you x amount of length through your book?
-#this will have to use dateobject aswell -- if day < 15 || day == 30
-#are you halfway through book
-#are you done the book
-#need to breakup datetime object and then refresh it once a day maybe?
+#goal is to create multiple different reminders based on date
+#halfway point
+#end of month
+#once i add !finished then i can @ only the people who arent done their books -- less channel spam
 @tasks.loop(hours=24)
 async def weekly_announcment():
-    message_channel= bot.get_channel(CHANNEL_ID)
-    # await message_channel.send("Are you reading your book? How are you enjoying it?")
+    messageChannel = bot.get_channel(CHANNEL_ID)
+    #on 15th sends reminder to read
     if currentDate.day == 15:
-        await message_channel.send("Today is the 6th of December")
-    else: 
-        await message_channel.send("Today is not the date you entered in the thingy")
+        await messageChannel.send(f"@everyone We are halfway through the month! Are you halfway through {bookOfTheMonth}?")
+    #
+    if currentDate.day == 30:
+        await messageChannel.send(f"@everyone The month is over hope you're done {bookOfTheMonth}!")
+    else:
+        await messageChannel.send(f"@everyone This is just a testing conditional I don't care what the date is makes sure loop is working.")
+
 
 @weekly_announcment.before_loop
 async def before():
