3b20c2fefd324b5865ba46db9ee99aa6696a9fd9
Brendan  Horton <brendanhorton@Brendans-MacBook-Pro.local>
2020-12-06 15:22:18 -0500

    added ignore and moved my token into it for public repo, began working with date object to customize my loops

--- Booker.py
+++ Booker.py
@@ -2,34 +2,25 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands, tasks
 import datetime
-
-# TODO
-# 1. seperate datetime object and begin using it for conditionals
-# 2. send progress reminders to users to keep them on track -- simple extension of loops once i have datetime object broken
-# 3. at end of month maybe i send a message and disscusion questions in order to stimulate conversation
-# 4. eventually get books to autochange and rotate through selections as new month hits
-# 4b) example: on 1st at 9am sends message >>> this month you are reading XYZ it is XYZ pages long, good luck have fun
-# 4c) maybe api implementation here? book api to pull book title and length maybe a brief sysnopsis even -- this could be fun
-
-#possible features?
-#add commands for check ins
-#add commands for finishing the book
-
-current_Date = datetime.datetime.now()
-
-#general channel id -- must have
-CHANNEL_ID = 784457938551046167
+from TOKEN import *
 
 #command prefix 
 bot = Bot(command_prefix='!')
-#my token
-TOKEN = 'Nzg0NDU2MDA2NTQ0MzkyMjc1.X8pjlg.FJeoVY8nwmG4o6OZ1SEbRpJyd1U'
+
+#intializes currentDate variable and update it when performing date based loops to ensure right time
+currentDate = datetime.datetime.now()
+
+#general channel id -- must have -- book club only has one channel so dont need to edit this rn
+#if expanded to work for others would need to have a different solution to finding channel id
+CHANNEL_ID = 784457938551046167
 
 # this event makes sure bot is ready and will output when it is
 #want to change bot so it displays the current months book // not sure how 
 #maybe i need to use a datetime object and conditionals
 #if month == x then output y as name of activity
-#doable
+#can have large hash with book for each month
+#expansion would include allowing user to set books for each month per instance of bot
+# doable
 @bot.event
 async def on_ready():
     print('bot running')
@@ -38,23 +29,23 @@ async def on_ready():
 #when someone types test bot returns testing 123
 @bot.event
 async def on_message(message):
-    if message.content == 'test':
-        await message.channel.send('Testing 1 2 3')
+    if message.content == '!currentbook':
+        await message.channel.send('The book for the month of December is Death on the Nile')
     
     await bot.process_commands(message)
 
 #simpel test command to learn discordloops
 #runs every 10sec
 #must have the before loop check to ready or it doesnt work
-@tasks.loop(seconds=10)
-async def testingLoop():
-    message_channel= bot.get_channel(CHANNEL_ID)
-    await message_channel.send("10 Second Bot Initiated")
+# @tasks.loop(seconds=10)
+# async def testingLoop():
+#     message_channel= bot.get_channel(CHANNEL_ID)
+#     await message_channel.send("10 Second Bot Initiated")
 
-@testingLoop.before_loop
-async def before():
-    await bot.wait_until_ready()
-    print("Finished waiting")
+# @testingLoop.before_loop
+# async def before():
+#     await bot.wait_until_ready()
+#     print("Finished waiting")
 
 #whiteboard this
 #want this loop to run once a week, provide motivation and remind for reading 
@@ -63,21 +54,23 @@ async def before():
 #are you halfway through book
 #are you done the book
 #need to breakup datetime object and then refresh it once a day maybe?
-@tasks.loop(hours=168)
+@tasks.loop(hours=24)
 async def weekly_announcment():
     message_channel= bot.get_channel(CHANNEL_ID)
-    await message_channel.send("Are you reading your book? How are you enjoying it?")
+    # await message_channel.send("Are you reading your book? How are you enjoying it?")
+    if currentDate.day == 15:
+        await message_channel.send("Today is the 6th of December")
+    else: 
+        await message_channel.send("Today is not the date you entered in the thingy")
 
 @weekly_announcment.before_loop
 async def before():
     await bot.wait_until_ready()
-    print("Weekly Bot Initiated")
+    currentDate = datetime.datetime.now()
+    print(f"updated datetime to {currentDate}")
 
-#prints date object
-print(current_Date)
-
-#need the start functions on loops or they dont run
-testingLoop.start()
+# #need the start functions on loops or they dont run
+# testingLoop.start()
 weekly_announcment.start()
 
 #must end with bot.run
