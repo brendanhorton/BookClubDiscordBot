40a97d3d51a605a219ae76452f5f3e77d41ad50f
Brendan  Horton <brendanhorton@Brendans-MacBook-Pro.local>
2020-12-14 17:25:38 -0500

    added finished command, need to refactor, a bit more OOP

--- Booker.py
+++ Booker.py
@@ -24,6 +24,7 @@ bookCollection = {
     "December": "death on the nile"
 }
 
+memberCollection = {}
 #command prefix and bot object
 bot = Bot(command_prefix='!')
 
@@ -54,10 +55,17 @@ async def on_ready():
     await bot.change_presence(activity= discord.Activity(type = discord.ActivityType.listening, name = bookCollection[currentMonth]))
 
 #provides title and author of current months book
+#should put this into variables for readability probably
 @bot.command()
-async def bookinfo(ctx):
+async def currentbook(ctx):
     await ctx.send(f"{response['results'][0]['title']} by {response['results'][0]['author']['name']}")
     await ctx.send(response['results'][0]['image_url'])
+
+
+@bot.command()
+async def bookinfo(ctx, month):
+    await ctx.send(f"{bookCollection[month]} is the book for the month of {month.capitalize()}")
+
 
 #command to addbook to a selected month
 @bot.command()
@@ -65,6 +73,27 @@ async def addbook(ctx, month, book):
     month = month.capitalize()
     bookCollection[month] = book
     await ctx.send(f"{book} has been chosen for the month of {month}")
+
+#gets all members in current channel and fills collection with names and book status(default unfinished -- may need to change)
+@bot.command()
+async def getmembers(ctx):
+    async for member in ctx.guild.fetch_members(limit=None):
+        if member.name == 'BookClubBot':
+            continue
+        memberCollection[member.name] = 'unfinished'
+    print(memberCollection)
+        
+@bot.command()
+async def bookfinished(ctx):
+    #get user who sent message
+    user = ctx.author.name
+    memberCollection[user] = 'finished'
+    #need function for checking if all finished
+    print(memberCollection)
+    if checkAllFinished(memberCollection):
+        await ctx.send('@everyone is finished their book! Time to discuss!')
+    #want to else here and send message which displays how many people haven't finished their book (if only 1 person then at them)
+    
 
 #for the boys
 @bot.command()
@@ -91,8 +120,23 @@ async def before():
     currentDate = datetime.datetime.now()
     print(f"updated datetime to {currentDate}")
 
+#function which gets checks if all members of guild have completed the book
+#can add way more functionality to this if i want
+#perhaps in seperate functions
+def checkAllFinished(memberCollection):
+    allFinished = True
+    for member in memberCollection:
+        if memberCollection[member] == 'unfinished':
+            allFinished = False
+    
+    return allFinished
+
 #need the start method on loops or they dont run
 weekly_announcment.start()
 
 #must end with bot.run
 bot.run(TOKEN)
+
+##TODO
+#need to refresh bookOfTheMonth if current month is changed
+#need api call for !bookinfo 
